618ee6fca995e3bd5edd6520a3b06c8069a89b39
shumatsuura <shunsuke.mtr@gmail.com>
2020-04-30 01:03:10 +0900

    [add]create language table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_29_160013) do
+ActiveRecord::Schema.define(version: 2020_04_29_160147) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(version: 2020_04_29_160013) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_educations_on_user_id"
+  end
+
+  create_table "languages", force: :cascade do |t|
+    t.string "name"
+    t.integer "level"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_languages_on_user_id"
   end
 
   create_table "tests", force: :cascade do |t|
@@ -61,4 +70,5 @@ ActiveRecord::Schema.define(version: 2020_04_29_160013) do
   end
 
   add_foreign_key "educations", "users"
+  add_foreign_key "languages", "users"
 end
