75c4039ce1840cf55896463a54a096c0fdcae792
shumatsuura <shunsuke.mtr@gmail.com>
2020-04-30 02:15:32 +0900

    [add]created post_industries table

---
+++ app/models/post_industry.rb
@@ -0,0 +1,2 @@
+class PostIndustry < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_29_170718) do
+ActiveRecord::Schema.define(version: 2020_04_29_171423) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,6 +83,13 @@ ActiveRecord::Schema.define(version: 2020_04_29_170718) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_languages_on_user_id"
+  end
+
+  create_table "post_industries", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "industry_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
