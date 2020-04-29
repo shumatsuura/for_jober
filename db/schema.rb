21e3b5b0b50a58828ce266c707e6b3f0cf942d2f
shumatsuura <shunsuke.mtr@gmail.com>
2020-04-30 01:25:41 +0900

    [add]created qualifications table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_29_162216) do
+ActiveRecord::Schema.define(version: 2020_04_29_162435) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,15 @@ ActiveRecord::Schema.define(version: 2020_04_29_162216) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_languages_on_user_id"
+  end
+
+  create_table "qualifications", force: :cascade do |t|
+    t.string "name"
+    t.datetime "date_of_acquisition"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_qualifications_on_user_id"
   end
 
   create_table "tests", force: :cascade do |t|
@@ -106,6 +115,7 @@ ActiveRecord::Schema.define(version: 2020_04_29_162216) do
 
   add_foreign_key "educations", "users"
   add_foreign_key "languages", "users"
+  add_foreign_key "qualifications", "users"
   add_foreign_key "user_skills", "users"
   add_foreign_key "work_experiences", "users"
 end
