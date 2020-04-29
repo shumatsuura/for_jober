aa1bcd00a9e69047958f0bb28c6a121f541235e0
shumatsuura <shunsuke.mtr@gmail.com>
2020-04-30 01:38:06 +0900

    [add]created desired_job_categories table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_29_162608) do
+ActiveRecord::Schema.define(version: 2020_04_29_163637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2020_04_29_162608) do
   create_table "desired_industries", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "industry_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "desired_job_categories", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "job_category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
---
+++ spec/factories/desired_job_categories.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :desired_job_category do
+    
+  end
+end
