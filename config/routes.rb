fc3f7fa70a85debaf6965e62a481fd86e431b346
shumatsuura <shunsuke.mtr@gmail.com>
2020-04-30 16:14:03 +0900

    [add]config, db, public

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   # mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   root 'statics#landing'
 
-  # devise_for :companies, controllers: {
-  #       sessions: 'companies/sessions'
-  #     }
+  devise_for :companies, controllers: {
+        sessions: 'companies/sessions'
+      }
 
   devise_for :users, controllers: {
         registrations: 'users/registrations',
@@ -30,66 +30,66 @@ Rails.application.routes.draw do
     end
   end
 
-  # namespace :admin do
-  #   resources :users do
-  #     collection do
-  #       get :dashboard
-  #     end
-  #   end
-  #
-  #   resources :companies
-  #   resources :posts, only:[:index, :destroy]
-  #   resources :scouts, only:[:index, :destroy] do
-  #     resources :scout_messages, only:[:index, :destroy]
-  #   end
-  #
-  #   resources :scout_messages, only:[] do
-  #     collection do
-  #       get :index_all
-  #     end
-  #   end
-  #
-  #   resources :applies, only:[:index, :destroy] do
-  #     resources :apply_messages, only:[:index, :destroy]
-  #   end
-  #
-  #   resources :apply_messages, only:[] do
-  #     collection do
-  #       get :index_all
-  #     end
-  #   end
-  # end
-  #
-  # resources :companies, only:[:show,:index,:edit,:update] do
-  #   member do
-  #     get :dashboard
-  #   end
-  # end
-  #
-  # resources :company_skills
-  #
-  # resources :posts do
-  #   member do
-  #     get :manage
-  #   end
-  # end
-  #
-  # resources :scouts, only:[:index, :create, :destroy] do
-  #   resources :scout_messages, only:[:index, :create]
-  # end
-  #
-  # resources :applies, only:[:index, :create,:edit,:update, :destroy] do
-  #   resources :apply_messages, only:[:index, :create]
-  # end
-  #
-  # resources :follows, only:[:index,:create,:destroy]
-  # resources :like_posts, only:[:index,:create,:destroy]
-  # resources :like_users, only:[:index,:create,:destroy]
-  #
-  # resources :contacts, only:[:new,:create]
-  # resources :notifications, only:[:index,:show] do
-  #   collection do
-  #     get :change_to_read
-  #   end
-  # end
+  namespace :admin do
+    resources :users do
+      collection do
+        get :dashboard
+      end
+    end
+
+    resources :companies
+    resources :posts, only:[:index, :destroy]
+    resources :scouts, only:[:index, :destroy] do
+      resources :scout_messages, only:[:index, :destroy]
+    end
+
+    resources :scout_messages, only:[] do
+      collection do
+        get :index_all
+      end
+    end
+
+    resources :applies, only:[:index, :destroy] do
+      resources :apply_messages, only:[:index, :destroy]
+    end
+
+    resources :apply_messages, only:[] do
+      collection do
+        get :index_all
+      end
+    end
+  end
+
+  resources :companies, only:[:show,:index,:edit,:update] do
+    member do
+      get :dashboard
+    end
+  end
+
+  resources :company_skills
+
+  resources :posts do
+    member do
+      get :manage
+    end
+  end
+
+  resources :scouts, only:[:index, :create, :destroy] do
+    resources :scout_messages, only:[:index, :create]
+  end
+
+  resources :applies, only:[:index, :create,:edit,:update, :destroy] do
+    resources :apply_messages, only:[:index, :create]
+  end
+
+  resources :follows, only:[:index,:create,:destroy]
+  resources :like_posts, only:[:index,:create,:destroy]
+  resources :like_users, only:[:index,:create,:destroy]
+
+  resources :contacts, only:[:new,:create]
+  resources :notifications, only:[:index,:show] do
+    collection do
+      get :change_to_read
+    end
+  end
 end
